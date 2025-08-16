2d83715e7ace2bfe454641fafabb22ee9063d61d
Tiwat <armlnw.58@gmail.com>
2025-08-16 10:27:27 +0700

    step5

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -4,20 +4,55 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.brown,
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        padding: const EdgeInsets.all(50),
-        child: Text(
-          "Hello Flutter! Step ",
-          style: TextStyle(
-            fontSize: 30,
-            color: const Color.fromARGB(255, 183, 80, 80),
-            fontWeight: FontWeight.bold,
-          ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+//mainAxisAlignment: MainAxisAlignment.start,
+//mainAxisAlignment: MainAxisAlignment.center,
+//mainAxisAlignment: MainAxisAlignment.end,
+//mainAxisAlignment: MainAxisAlignment.spaceAround,
+//mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          color:  Colors.redAccent,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical:  5),
+          child: Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3,
+            ),
+          )
         ),
-      ),
+          Container(
+          color:  Colors.redAccent,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical:  5),
+          child: Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3,
+            ),
+          )
+        ),
+          Container(
+          color:  Colors.redAccent,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical:  5),
+          child: Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3,
+            ),
+          )
+        ),
+      ],
     );
+    
   }
 }
